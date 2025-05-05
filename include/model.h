9f0a79d8560a19b580b483beed44dc58bfc80ad6
haolamnm <haolamnm@gmail.com>
2025-05-05 21:02:14 +0700

    Update: Use shared pointer for Population

--- include/model.h
+++ include/model.h
@@ -1,6 +1,7 @@
 #ifndef MODEL_H
 #define MODEL_H
 
+#include <memory>
 #include <string>
 #include <vector>
 
@@ -12,16 +13,18 @@
  * */
 class Model {
    private:
-    int remain_days = 0;    ///< Remaining days until the simulation finishes
-    int current_day = 1;    ///< Current day of the simulation
-    Population population;  ///< Population being simulated
-    std::string name = "";  ///< Name of the model
+    int remain_days = 0;                     ///< Remaining days until the simulation finishes
+    int current_day = 1;                     ///< Current day of the simulation
+    std::shared_ptr<Population> population;  ///< Shared pointer to population being simulated
+    std::string name = "";                   ///< Name of the model
 
     std::vector<std::vector<std::vector<int>>>
         data;                             ///< 3D vector of population states for each day
     std::vector<std::vector<int>> stats;  ///< 2D vector of status counts for each day
 
    public:
+    Model(int days_in_simulation, std::shared_ptr<Population> population,
+          const std::string &name = "");
     Model(int days_in_simulation, int size, int travel_radius, int encounters, int init_incubations,
           int init_infections, const Disease &disease, const std::string &name = "");
 
@@ -29,9 +32,12 @@ class Model {
 
     const std::vector<std::vector<std::vector<int>>> &get_data() const;
     const std::vector<std::vector<int>> &get_stats() const;
-    const Population &get_population() const;
+    std::shared_ptr<Population> get_population() const;
     int get_remain_days() const;
     int get_current_day() const;
+    std::string get_name() const;
+
+    void set_name(const std::string &name);
 
    private:
 };
--- src/model.cpp
+++ src/model.cpp
@@ -1,13 +1,30 @@
 #include "model.h"
 
+#include <memory>
 #include <stdexcept>
+#include <string>
+
+#include "disease.h"
+#include "population.h"
+
+Model::Model(int days_in_simulation, std::shared_ptr<Population> population,
+             const std::string &name)
+    : remain_days(days_in_simulation), population(std::move(population)), name(name) {
+    if (days_in_simulation < 0) {
+        throw std::invalid_argument("Days in simulation must be non-negative");
+    }
+    // Reserve all the necessary memory
+    data.reserve(days_in_simulation);
+    stats.reserve(days_in_simulation);
+}
 
 Model::Model(int days_in_simulation, int size, int travel_radius, int encounters,
-             int init_incubations, int init_infections, const Disease& disease,
-             const std::string& name)
+             int init_incubations, int init_infections, const Disease &disease,
+             const std::string &name)
     : remain_days(days_in_simulation),
-      population(size, travel_radius, encounters, init_incubations, init_infections, disease,
-                 name) {
+      population(std::make_shared<Population>(size, travel_radius, encounters, init_incubations,
+                                              init_infections, disease)),
+      name(name) {
     if (days_in_simulation < 0) {
         throw std::invalid_argument("Days in simulation must be non-negative");
     }
@@ -26,9 +43,9 @@ bool Model::simulate(int days = -1) {
     if (days <= 0) return false;
 
     for (int d = 1; d <= days; ++d) {
-        population.update();
-        data.push_back(population.get_people());
-        stats.push_back(population.get_status_count());
+        population->update();
+        data.push_back(population->get_people());
+        stats.push_back(population->get_status_count());
     }
     remain_days -= days;
     current_day += days;
@@ -36,15 +53,15 @@ bool Model::simulate(int days = -1) {
     return true;
 }
 
-const std::vector<std::vector<std::vector<int>>>& Model::get_data() const {
+const std::vector<std::vector<std::vector<int>>> &Model::get_data() const {
     return data;
 }
 
-const std::vector<std::vector<int>>& Model::get_stats() const {
+const std::vector<std::vector<int>> &Model::get_stats() const {
     return stats;
 }
 
-const Population& Model::get_population() const {
+std::shared_ptr<Population> Model::get_population() const {
     return population;
 }
 
@@ -54,4 +71,12 @@ int Model::get_current_day() const {
 
 int Model::get_remain_days() const {
     return remain_days;
+}
+
+std::string Model::get_name() const {
+    return name;
+}
+
+void Model::set_name(const std::string &name) {
+    this->name = name;
 }
