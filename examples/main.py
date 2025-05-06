2ecae4c3542ef9bafe54c1ae487c3dbdc5962a67
haolamnm <haolamnm@gmail.com>
2025-05-06 23:42:22 +0700

    Update: Intense example main

--- examples/main.py
+++ examples/main.py
@@ -7,25 +7,25 @@ from ssir import Disease, Model, Population  # noqa: E402
 
 disease = Disease(
     transmission_rate=0.4,
-    fatality_rate=0.01,
-    days_in_incubation=7,
-    days_with_symptoms=8,
+    fatality_rate=0.02,
+    days_in_incubation=12,
+    days_with_symptoms=14,
     name="Virus",
 )
 pop = Population(
-    size=50,
-    travel_radius=3,
-    encounters=4,
+    size=300,
+    travel_radius=10,
+    encounters=7,
     init_incubations=3,
     init_infections=1,
     disease=disease,
     name="City",
 )
 model = Model(
-    days_in_simulation=200,
+    days_in_simulation=150,
     population=pop,
     name="Model",
 )
 
 model.simulate(days=200)
-show_simulation(model)
+show_simulation(model, save_path="simulation.gif", show=False)
